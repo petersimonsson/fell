b127377201789c357158f67e058e7383d8075d74
Peter Simonsson <peter.simonsson@gmail.com>
2024-08-14 13:07:50 +0200

    Make clippy happy

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use std::{ffi::CStr, mem, ptr};
 pub fn get_username_from_uid(uid: u32) -> Option<String> {
     unsafe {
         let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
-            n if n < 0 => 512 as usize,
+            n if n < 0 => 512_usize,
             n => n as usize,
         };
         let mut buf = Vec::with_capacity(amt);

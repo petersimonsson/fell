67054e9a0017f15968740b7422dc1d7d617cf571
Peter Simonsson <peter.simonsson@gmail.com>
2024-09-26 10:55:15 +0200

    Calculate CPU usage for process

--- src/proc/mod.rs
+++ src/proc/mod.rs
@@ -1,6 +1,11 @@
 mod stat;
 
-use std::{fmt::Display, fs, path::Path};
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    fs,
+    path::{Path, PathBuf},
+};
 
 use stat::Stat;
 use thiserror::Error;
@@ -11,9 +16,15 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error("Failed to parse stat")]
     StatParsing(String),
+    #[error("Failed to read uptime")]
+    Uptime(String),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Proc {
+    prev_cpus: HashMap<i32, PrevCpu>,
+}
 
 #[derive(Default, Debug)]
 pub struct System {
@@ -35,7 +46,7 @@ pub struct ProcessInfo {
     pub state: State,
     pub memory: u64,
     pub virtual_memory: u64,
-    pub cpu_usage: f32,
+    pub cpu_usage: Option<f32>,
     pub cmdline: String,
     pub process_type: ProcessType,
     pub num_threads: u32,
@@ -49,36 +60,52 @@ pub enum ProcessType {
     KernelThread,
 }
 
-pub fn get_system() -> Result<System> {
-    let dir_iter = fs::read_dir("/proc")?;
-    let mut processes = Vec::new();
-    let mut num_threads = ThreadCount::default();
-
-    for entry in dir_iter.flatten() {
-        if let Ok(name) = entry.file_name().into_string() {
-            if let Ok(pid) = name.parse::<i32>() {
-                if let Some(info) = read_process_info(pid, &entry.path())? {
-                    if let ProcessType::KernelThread = info.process_type {
-                        num_threads.kernel_threads += 1;
-                    } else {
-                        num_threads.tasks += 1;
-                    }
-
-                    num_threads.threads += info.num_threads - 1;
-
-                    processes.push(info);
-                }
-            }
+impl Proc {
+    pub fn new() -> Self {
+        Proc {
+            prev_cpus: HashMap::default(),
         }
     }
 
-    Ok(System {
-        processes,
-        num_threads,
-    })
+    pub fn get_system(&mut self) -> Result<System> {
+        let dir_iter = fs::read_dir("/proc")?;
+        let mut processes = Vec::new();
+        let mut num_threads = ThreadCount::default();
+        let uptime = read_uptime("/proc/uptime".into())?;
+
+        for entry in dir_iter.flatten() {
+            if let Ok(name) = entry.file_name().into_string() {
+                if let Ok(pid) = name.parse::<i32>() {
+                    if let Some(info) =
+                        read_process_info(pid, &entry.path(), uptime, &mut self.prev_cpus)?
+                    {
+                        if let ProcessType::KernelThread = info.process_type {
+                            num_threads.kernel_threads += 1;
+                        } else {
+                            num_threads.tasks += 1;
+                        }
+
+                        num_threads.threads += info.num_threads - 1;
+
+                        processes.push(info);
+                    }
+                }
+            }
+        }
+
+        Ok(System {
+            processes,
+            num_threads,
+        })
+    }
 }
 
-fn read_process_info(pid: i32, path: &Path) -> Result<Option<ProcessInfo>> {
+fn read_process_info(
+    pid: i32,
+    path: &Path,
+    uptime: f64,
+    prev_cpus: &mut HashMap<i32, PrevCpu>,
+) -> Result<Option<ProcessInfo>> {
     if let Ok(stat) = fs::read_to_string(path.join("stat")) {
         let cmdline = fs::read_to_string(path.join("cmdline"))
             .unwrap_or_default()
@@ -100,7 +127,7 @@ fn read_process_info(pid: i32, path: &Path) -> Result<Option<ProcessInfo>> {
             state: stat.state,
             memory: stat.memory_res,
             virtual_memory: stat.memory_virtual,
-            cpu_usage: stat.cpu_used as f32,
+            cpu_usage: prev_cpus.calculate(pid, uptime, stat.cpu_used),
             cmdline,
             process_type,
             num_threads: stat.num_threads,
@@ -108,6 +135,22 @@ fn read_process_info(pid: i32, path: &Path) -> Result<Option<ProcessInfo>> {
     } else {
         Ok(None)
     }
+}
+
+fn read_uptime(path: PathBuf) -> Result<f64> {
+    let uptime = fs::read_to_string(&path).map_err(|_| {
+        Error::Uptime(format!(
+            "Could not find an uptime file at {}",
+            path.display()
+        ))
+    })?;
+    let (uptime, _) = uptime
+        .split_once(' ')
+        .ok_or_else(|| Error::Uptime("Failed to split string".to_string()))?;
+
+    uptime
+        .parse::<f64>()
+        .map_err(|_| Error::Uptime("Failed to parse uptime to f64".to_string()))
 }
 
 #[derive(Default, Debug)]
@@ -153,13 +196,39 @@ impl Display for State {
     }
 }
 
+struct PrevCpu {
+    uptime: f64,
+    cpu_used: u32,
+}
+
+trait PrevCpuMap {
+    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u32) -> Option<f32>;
+}
+
+impl PrevCpuMap for HashMap<i32, PrevCpu> {
+    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u32) -> Option<f32> {
+        if let Some(prev_cpu) = self.get_mut(&pid) {
+            let cpu_usage = (cpu_used - prev_cpu.cpu_used) as f64 / (uptime - prev_cpu.uptime);
+            prev_cpu.uptime = uptime;
+            prev_cpu.cpu_used = cpu_used;
+
+            Some(cpu_usage as f32)
+        } else {
+            self.insert(pid, PrevCpu { uptime, cpu_used });
+
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn get_pids() -> Result<()> {
-        let system = get_system()?;
+        let mut proc = Proc::new();
+        let system = proc.get_system()?;
 
         println!("{:?}", system);
 

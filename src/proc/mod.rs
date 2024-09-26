daf6f57385e20d2e0e0ac435067a039ef83ffedd
Peter Simonsson <peter.simonsson@gmail.com>
2024-09-26 13:01:57 +0200

    Use rustix to get page_size and ticks

--- src/proc/mod.rs
+++ src/proc/mod.rs
@@ -23,6 +23,8 @@ pub enum Error {
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub struct Proc {
+    ticks: u64,
+    page_size: usize,
     prev_cpus: HashMap<i32, PrevCpu>,
 }
 
@@ -44,8 +46,8 @@ pub struct ProcessInfo {
     pub pid: i32,
     pub name: String,
     pub state: State,
-    pub memory: u64,
-    pub virtual_memory: u64,
+    pub memory: usize,
+    pub virtual_memory: usize,
     pub cpu_usage: Option<f32>,
     pub cmdline: String,
     pub process_type: ProcessType,
@@ -62,7 +64,11 @@ pub enum ProcessType {
 
 impl Proc {
     pub fn new() -> Self {
+        let ticks = rustix::param::clock_ticks_per_second();
+        let page_size = rustix::param::page_size();
         Proc {
+            ticks,
+            page_size,
             prev_cpus: HashMap::default(),
         }
     }
@@ -76,9 +82,7 @@ impl Proc {
         for entry in dir_iter.flatten() {
             if let Ok(name) = entry.file_name().into_string() {
                 if let Ok(pid) = name.parse::<i32>() {
-                    if let Some(info) =
-                        read_process_info(pid, &entry.path(), uptime, &mut self.prev_cpus)?
-                    {
+                    if let Some(info) = self.read_process_info(pid, &entry.path(), uptime)? {
                         if let ProcessType::KernelThread = info.process_type {
                             num_threads.kernel_threads += 1;
                         } else {
@@ -100,42 +104,44 @@ impl Proc {
             num_threads,
         })
     }
-}
 
-fn read_process_info(
-    pid: i32,
-    path: &Path,
-    uptime: f64,
-    prev_cpus: &mut HashMap<i32, PrevCpu>,
-) -> Result<Option<ProcessInfo>> {
-    if let Ok(stat) = fs::read_to_string(path.join("stat")) {
-        let cmdline = fs::read_to_string(path.join("cmdline"))
-            .unwrap_or_default()
-            .replace('\0', " ")
-            .trim()
-            .to_string();
+    fn read_process_info(
+        &mut self,
+        pid: i32,
+        path: &Path,
+        uptime: f64,
+    ) -> Result<Option<ProcessInfo>> {
+        if let Ok(stat) = fs::read_to_string(path.join("stat")) {
+            let cmdline = fs::read_to_string(path.join("cmdline"))
+                .unwrap_or_default()
+                .replace('\0', " ")
+                .trim()
+                .to_string();
 
-        let process_type = if cmdline.is_empty() {
-            ProcessType::KernelThread
+            let process_type = if cmdline.is_empty() {
+                ProcessType::KernelThread
+            } else {
+                ProcessType::Task
+            };
+
+            let stat = Stat::parse(&stat)?;
+
+            Ok(Some(ProcessInfo {
+                pid,
+                name: stat.name,
+                state: stat.state,
+                memory: stat.memory_res as usize * self.page_size,
+                virtual_memory: stat.memory_virtual,
+                cpu_usage: self
+                    .prev_cpus
+                    .calculate(pid, uptime, stat.cpu_used, self.ticks),
+                cmdline,
+                process_type,
+                num_threads: stat.num_threads,
+            }))
         } else {
-            ProcessType::Task
-        };
-
-        let stat = Stat::parse(&stat)?;
-
-        Ok(Some(ProcessInfo {
-            pid,
-            name: stat.name,
-            state: stat.state,
-            memory: stat.memory_res,
-            virtual_memory: stat.memory_virtual,
-            cpu_usage: prev_cpus.calculate(pid, uptime, stat.cpu_used),
-            cmdline,
-            process_type,
-            num_threads: stat.num_threads,
-        }))
-    } else {
-        Ok(None)
+            Ok(None)
+        }
     }
 }
 
@@ -203,18 +209,19 @@ impl Display for State {
 
 struct PrevCpu {
     uptime: f64,
-    cpu_used: u32,
+    cpu_used: u64,
 }
 
 trait PrevCpuMap {
-    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u32) -> Option<f32>;
+    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u64, ticks: u64) -> Option<f32>;
     fn cleanup(&mut self, uptime: f64);
 }
 
 impl PrevCpuMap for HashMap<i32, PrevCpu> {
-    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u32) -> Option<f32> {
+    fn calculate(&mut self, pid: i32, uptime: f64, cpu_used: u64, ticks: u64) -> Option<f32> {
         if let Some(prev_cpu) = self.get_mut(&pid) {
-            let cpu_usage = (cpu_used - prev_cpu.cpu_used) as f64 / (uptime - prev_cpu.uptime);
+            let cpu_usage =
+                ((cpu_used - prev_cpu.cpu_used) * ticks) as f64 / (uptime - prev_cpu.uptime);
             prev_cpu.uptime = uptime;
             prev_cpu.cpu_used = cpu_used;
 
